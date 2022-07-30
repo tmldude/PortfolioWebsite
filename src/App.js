f843c96091342b7a211d27f856210fda84bae1d8
teddy lazar <tl688@cornell.edu>
2022-07-30 16:30:56 -0400

    CSS workshop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Fragment } from 'react';
+
+import Header from "./Components/Header/Header";
+import DiagonalBox from "./Components/DiagonalBox";
+import TriangleAtBottom from './Components/Bottom/TriangleAtBottom';
+
 function App() {
-    return (
-      <div>
-        <h2>Let's get started!</h2>
-      </div>
-    );
-  }
-  
-  export default App;
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <DiagonalBox />
+      </main>
+      <TriangleAtBottom/>
+    </Fragment>
+  );
+}
+
+export default App;
