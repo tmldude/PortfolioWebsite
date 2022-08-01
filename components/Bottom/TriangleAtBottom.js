4221b48cb91b2e663cbd3a7a59fbd29ce1e86ab5
teddy lazar <tl688@cornell.edu>
2022-08-01 17:33:35 -0400

    Renamed Components folder

---
+++ components/Bottom/TriangleAtBottom.js
@@ -0,0 +1,9 @@
+import classes from './TriangleAtBottom.module.css';
+
+const TriangleAtBottom = () => {
+    return (
+        <footer className={classes.triangle}></footer>
+    )
+}
+
+export default TriangleAtBottom;
