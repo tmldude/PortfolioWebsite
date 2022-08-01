ccaa4d47411db783af2f85a0edc62d2630f49fcb
Theodore Lazar <52608372+tmldude@users.noreply.github.com>
2022-08-01 17:32:33 -0400

    Delete src/Components directory

--- src/Components/Bottom/TriangleAtBottom.js
+++
@@ -1,9 +0,0 @@
-import classes from './TriangleAtBottom.module.css';
-
-const TriangleAtBottom = () => {
-    return (
-        <footer className={classes.triangle}></footer>
-    )
-}
-
-export default TriangleAtBottom;
